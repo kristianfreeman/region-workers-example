922a54a9e8676b71c17e8870406a4bd669d6dee0
Kristian Freeman <kristian@kristianfreeman.com>
2020-12-03 10:39:18 -0600

    Update template with HTML layout

--- template.js
+++ template.js
@@ -5,12 +5,32 @@ const template = `
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-
     <title>Hello!</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
+
+    <style>
+      body {
+        background: #eee;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        font-family: sans-serif;
+      }
+
+      div.container {
+        background: #fff;
+        border-radius: 1rem;
+        padding: 4rem;
+      }
+    </style>
   </head>
 
   <body>
-    <h1>Hello world 👋🏻</h1>
+    <div class="container">
+      <h1>Hello there! 👋🏻</h1>
+    </div>
   </body>
 </html>
 `
