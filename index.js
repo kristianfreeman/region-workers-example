91f1fcbe480f609a7afe3aa2107ed550d945bb09
Kristian Freeman <kristian@kristianfreeman.com>
2020-12-03 10:09:00 -0600

    Render an HTML page

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+import template from './template'
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+  return new Response(template, {
+    headers: { 'content-type': 'text/html' },
   })
 }
