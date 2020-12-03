32f849a9dd792c7e8f7aaeb3664b0ff01f66cfa9
Kristian Freeman <kristian@kristianfreeman.com>
2020-12-03 11:00:21 -0600

    Show region text to indicate where a request is coming from

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
-  return new Response(template, {
+  return new Response(template(request.cf), {
     headers: { 'content-type': 'text/html' },
   })
 }
--- template.js
+++ template.js
@@ -1,38 +1,44 @@
-const template = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Hello!</title>
+import flag from 'country-code-emoji'
 
-    <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
+const template = cf => {
+  const emoji = flag(cf.country) || "👋🏻"
 
-    <style>
-      body {
-        background: #eee;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        min-height: 100vh;
-        font-family: sans-serif;
-      }
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Hello!</title>
 
-      div.container {
-        background: #fff;
-        border-radius: 1rem;
-        padding: 4rem;
-      }
-    </style>
-  </head>
+        <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
 
-  <body>
-    <div class="container">
-      <h1>Hello there! 👋🏻</h1>
-    </div>
-  </body>
-</html>
-`
+        <style>
+          body {
+            background: #eee;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+            font-family: sans-serif;
+          }
+
+          div.container {
+            background: #fff;
+            border-radius: 1rem;
+            padding: 4rem;
+          }
+        </style>
+      </head>
+
+      <body>
+        <div class="container">
+          <h1>Hello there! You're connecting from ${cf.country} ${emoji}</h1>
+        </div>
+      </body>
+    </html>
+    `
+}
 
 export default template
